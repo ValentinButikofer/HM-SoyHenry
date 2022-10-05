f227e838fade89e96f6f9aabf69d076e5999af04
ValentinButikofer <103600514+ValentinButikofer@users.noreply.github.com>
2022-10-05 23:49:02 +0200

    Estructura de datos II

--- 05-EstructuraDeDatos-II/homework/homework.js
+++ 05-EstructuraDeDatos-II/homework/homework.js
@@ -11,9 +11,60 @@ Implementar la clase LinkedList, definiendo los siguientes métodos:
   En caso de que la búsqueda no arroje resultados, search debe retornar null.
 */
 
-function LinkedList() {}
+function LinkedList(){
+  this.length = 0
+  this.head = null
+}
 
-function Node(value) {}
+function Node(value){
+  this.value = value
+  this.next = null
+}
+
+LinkedList.prototype.add = function(value){
+    var node1 = new Node(value)
+    var current = this.head // asigna el valor de la variable al head para usarlo como referencia 
+    if (!current) { // el head hace referencia al primer slot y el current a donde estamos parados
+      this.head = node1 // si no hay ningun nodo agrega nuestro nuevo nodo a la primer posicion / head
+      this.length++
+      return // hay que ponerlo para que haya un corte
+    }
+    while (current.next !== null){
+    current = current.next // este while es el que hace que el cursor avance
+    }    
+    current.next = node1 // cuando la condicion del while no se cumpla, osea que el sig slot esta vacio, entonces indicamos que nuestro nuevo nodo se ubique ahi
+}
+
+LinkedList.prototype.remove = function(){
+var current = this.head
+if(!current) return null
+if(!current.next){
+  this.head = null //quita el primer elemento 
+  return current.value //retorna el valor que tenia el elemento que quitaste                        
+}                             // 1 -----2 -----3----- 4------ 5------ 6                                                                       
+while(current.next.next){     //                      |
+  current = current.next
+}
+var aux = current.next //estas lineas de codigo albergan el valor del ultimo slot en una variable para ser devuelto
+current.next = null // aca se elimina y el current.next desaparece
+return aux.value
+}
+
+LinkedList.prototype.search = function(valor){
+var current = this.head 
+if(!current) return null
+while(current){ //mientras que haya algun nodo existente entra a examinar esto
+  if(typeof valor === "function") { // para que el metodo search reconozca tambien functions y strings 
+    if(valor(current.value)){ // si el valor ingresado para la busqueda coincide con el valor que se esta examinando al momento
+      return current.value // retorna ese valor y corta la lectura
+    }
+  }
+  if (valor === current.value) return current.value //aca dice que si coincide deberia devolver el valor porque se encontro
+  current = current.next // de lo contrario seguir iterando 
+}
+return null //si sale del while y del if sin ningun resultado true en la comparacion debe devolver que no se encontro
+}
+
 
 /*
 Implementar la clase HashTable.
@@ -30,7 +81,35 @@ La clase debe tener los siguientes métodos:
 Ejemplo: supongamos que quiero guardar {instructora: 'Ani'} en la tabla. Primero puedo chequear, con hasKey, si ya hay algo en la tabla con el nombre 'instructora'; luego, invocando set('instructora', 'Ani'), se almacenará el par clave-valor en un bucket específico (determinado al hashear la clave)
 */
 
-function HashTable() {}
+function HashTable(){
+  this.numBuckets = 35
+  this.buckets = []
+  for(let i = 0; i < 35; i++){
+    this.buckets.push({})
+  }
+}
+HashTable.prototype.set = function(key, value){ //asignado en la HM que debia estar con clave valor 
+if(typeof key !== "string") throw new TypeError ("Keys must be strings") // asignado en la HM que el set debe retornar solo strings
+var index = this.hash(key)
+
+this.buckets[index][key] = value
+}
+HashTable.prototype.get = function(key){
+  var index = this.hash(key)
+  return this.buckets[index][key]
+}
+HashTable.prototype.hasKey = function(key){
+ if(typeof key !== "string") throw new TypeError ("Keys must be strings")
+ var index = this.hash(key)
+ return this.buckets[index].hasOwnProperty(key)
+}
+HashTable.prototype.hash = function(v){
+  var res = 0
+  for (let i = 0; i < v.length; i++){
+  res = res + v.charCodeAt(i)
+  }
+  return res % this.numBuckets // se usa para retornar el resto mientras que este dentro de numBuckets que es = 35
+}
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
